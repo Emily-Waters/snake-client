442d442795791ee9151d8c509d6cdbe4727c0ef1
Emily-Waters <emilyabigailwaters@gmail.com>
2021-12-01 20:40:21 +0000

    moved input stuff from play to input

--- play.js
+++ play.js
@@ -1,28 +1,6 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
-
-
-// setup interface to handle user input from stdin
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", data => {
-    return handleUserInput(data);
-  });
-  return stdin;
-};
-
-
-const handleUserInput = function(key) {
-  // your code here
-  if (key === '\u0003') {
-    process.exit();
-  }
-  
-};
 
 setupInput();
 
