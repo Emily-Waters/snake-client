145caf8982253b2889ff7839a18eb45efeda09fa
Emily-Waters <emilyabigailwaters@gmail.com>
2021-12-01 18:23:53 +0000

    Implemented setupInput and handleUserInput with quit command functionality

--- play.js
+++ play.js
@@ -1,3 +1,29 @@
-const connect = require('./client');
+const { connect } = require('./client');
+
+
+
+// setup interface to handle user input from stdin
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", data => {
+    return handleUserInput(data);
+  });
+  return stdin;
+};
+
+
+const handleUserInput = function(key) {
+  // your code here
+  if (key === '\u0003') {
+    process.exit();
+  }
+  
+};
+
+setupInput();
 
 connect();
